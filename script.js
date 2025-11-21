76029cd4b1e35ec8f17c00f7c9061a95397362f5
Brendenbean <BWells5@cps.edu>
2025-11-21 10:34:05 +0000

    wrgwg

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ if(number * number2 === 4){
 console.log(point);
 document.body.style.margin = "0";
 document.body.style.height = "100vh";
-document.body.style.background = "linear-gradient(180deg, #ff9a3c 0%, #ff5f6d 40%, #d45079 70%, #662d8c 100%)";
+document.body.style.background = "linear-gradient(250deg, #ff9a3c 0%, #ff5f6d 40%, #d45079 70%, #662d8c 100%)";
   console.log("correct")
   
 }else{
@@ -55,7 +55,7 @@ if(third === "his"){
 console.log(point);
   console.log("correct")
     document.body.style.background = 
-    "linear-gradient(120deg, #8b0000, #ff8c00, #cd853f)";
+    "linear-gradient(250deg, #8b0000, #ff8c00, #cd853f)";
   document.body.style.minHeight = "100vh";
 }else{
    Score(-1);
@@ -81,7 +81,7 @@ console.log(point);
 document.body.style.height = "100vh";
 
 document.body.style.background =
-  "linear-gradient(180deg, #c40000 0%, #ffffff 50%, #0033a0 100%)";
+  "linear-gradient(250deg, #c40000 0%, #ffffff 50%, #0033a0 100%)";
 
 document.body.style.backgroundSize = "100% 100%";
 }else{
@@ -101,7 +101,7 @@ firstAnswer.addEventListener('change', function() {
 Score(1);
 console.log(point);
 document.body.style.background = 
-    "linear-gradient(150deg, #E487B1, #e100ffff, #3fcd7aff)";
+    "linear-gradient(250deg, #E487B1, #e100ffff, #3fcd7aff)";
   document.body.style.minHeight = "100vh";
   }
 });
@@ -174,7 +174,7 @@ Score(1);
 console.log(point);
 document.body.style.margin = "0";
 document.body.style.height = "100vh";
-document.body.style.background = "linear-gradient(135deg, #b30000 0%, #e63946 35%, #1b5e20 70%, #f7d774 100%)";
+document.body.style.background = "linear-gradient(250deg, #b30000 0%, #e63946 35%, #1b5e20 70%, #f7d774 100%)";
 document.body.style.backgroundSize = "100% 100%";
 });
 
@@ -189,7 +189,7 @@ document.body.style.position = "relative";
 document.body.style.overflow = "hidden";
 
 document.body.style.background =
-  "linear-gradient(180deg, #dff3ff 0%, #b9e1ff 35%, #8ecaff 70%, #66aee6 100%)";
+  "linear-gradient(250deg, #dff3ff 0%, #b9e1ff 35%, #8ecaff 70%, #66aee6 100%)";
 document.body.style.backgroundSize = "100% 100%";
 
 let sCount = 0;
